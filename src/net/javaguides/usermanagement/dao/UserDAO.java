f12098ceae84266bc8c97e83c1b4db045e8da32c
mikalyoungie <56412335+mikalyoungie@users.noreply.github.com>
2021-02-16 10:51:33 -0500

    Fixed internal 500 error in userDao()

--- src/net/javaguides/usermanagement/dao/UserDAO.java
+++ src/net/javaguides/usermanagement/dao/UserDAO.java
@@ -137,6 +137,8 @@ public class UserDAO {
             }
         } catch (SQLException e) {
             printSQLException(e);
+        }catch(NullPointerException v) {
+        	v.printStackTrace();
         }
         return users;
     }
